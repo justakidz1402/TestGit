9ec7a91cb0d96b8c80ace2cfd7618dbb38578914
Nguyễn Đăng Anh Đức <83125962+justakidz1402@users.noreply.github.com>
2022-07-03 11:06:15 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.collection').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev"><</button>',
+    nextArrow:'<button type="button" class="slick-prev">></button>',
+});
